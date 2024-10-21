bf17423785956d11effa340ebd50696236e303a1
R0GGER <8298741+R0GGER@users.noreply.github.com>
2024-10-22 00:44:12 +0200

    Update findtest.sh

--- findtest.sh
+++ findtest.sh
@@ -81,8 +81,9 @@ for ip in $SERVER_IPS; do
     # Calculate the distance between the client and the server
     distance=$(haversine $CLIENT_LAT $CLIENT_LON $SERVER_LAT $SERVER_LON)
 
-    #echo "Server $ip is $distance km away"
-
+    echo "Please wait... searching...."
+    # echo "Server $ip is $distance km away"
+    
     # Compare distances to find the closest server
     if [[ -z "$closest_distance" || $(echo "$distance < $closest_distance" | bc -l 2>/dev/null) -eq 1 ]]; then
         closest_distance=$distance
